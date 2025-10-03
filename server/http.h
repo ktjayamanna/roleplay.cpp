4e7a7a6a4d55477567aae3d705517eb19b064b01
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-03 21:32:08 +0000

    http header implemented

--- server/http.h
+++ server/http.h
@@ -21,12 +21,26 @@
  */
 
 // TODO: Add header guards
+#ifndef HTTP_H
+#define HTTP_H
+
 
 // TODO: Define HTTP status code constants
 
+const int HTTP_OK = 200;
+const int HTTP_NOT_FOUND = 404;
+
 // TODO: Define HttpResponse struct
 
+typedef struct {
+    int status_code;
+    char* body;
+    int body_length;
+} HttpResponse;
+
 // TODO: Declare function prototypes
+char* http_build_response(int status_code, const char* body);
+void http_parse_request(const char* request, char* method, char* path);
 
 // TODO: Close header guard
-
+#endif
