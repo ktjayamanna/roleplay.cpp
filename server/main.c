7257ff2bd72a4b1d13d4e343b2414ea29ad604e6
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-12 01:52:14 +0000

    completed server files

--- server/main.c
+++ server/main.c
@@ -18,17 +18,29 @@
  */
 
 // TODO: Add your includes here
+#include <server.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // TODO: Define PORT constant (8080)
 
 int main(int argc, char *argv[]) {
     // TODO: Print startup message
+    printf("Server starting on port 8080...\n");
     
     // TODO: Initialize server
+    if (server_init(8080) == -1) {
+        return 1;
+    }
     
     // TODO: Start server (this should block and run forever)
+    server_start();
+    server_stop();
+    printf("Server stopped\n");
     
     // TODO: Handle cleanup (if server stops)
+    // No cleanup needed for this basic example
+    
     
     return 0;
 }
