d5d035c0c24158dd006dd3be3ed41fa8d1e9fb3b
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-10-12 01:21:21 +0000

    server is implemented

--- server/server.c
+++ server/server.c
@@ -41,14 +41,89 @@
  */
 
 // TODO: Add your includes here
+#include "server.h"
+#include "http.h"
+#include "routes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 // TODO: Create global Server instance
+Server server;
+
 
 // TODO: Implement server_init()
+int server_init(int port) {
+    server.port = port;
+    server.is_running = 0;
+    server.socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server.socket_fd == -1) {
+        perror("socket");
+        return -1;
+    }
+    int opt = 1;
+    if (setsockopt(server.socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror("setsockopt");
+        return -1;
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(server.socket_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        return -1;
+    }
+    if (listen(server.socket_fd, 10) == -1) {
+        perror("listen");
+        return -1;
+    }
+    return 0;
+}
 
 // TODO: Implement server_start()
 
+int server_start() {
+    server.is_running = 1;
+    while (server.is_running) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server.socket_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_fd == -1) {
+            perror("accept");
+            continue;
+        }
+        handle_client(client_fd);
+        close(client_fd);
+    }
+    return 0;
+}
+
 // TODO: Implement server_stop()
 
+void server_stop() {
+    server.is_running = 0;
+    close(server.socket_fd);
+}
+
 // TODO: Implement handle_client()
+
+void handle_client(int client_fd) {
+    char buffer[1024];
+    ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read <= 0) {
+        return;
+    }
+    buffer[bytes_read] = '\0';
+    char method[10], path[100];
+    http_parse_request(buffer, method, path);
+    char* response = handle_route(method, path);
+    write(client_fd, response, strlen(response));
+    free(response);
+}
 
